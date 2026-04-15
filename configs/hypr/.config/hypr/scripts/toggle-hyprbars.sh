5a0fe0b22478b075e18eb239d6b6fdb5e2dff0f1
Pikalev15 <pehlevi224@gmail.com>
2026-04-15 14:34:09 +0800

    done?

--- configs/hypr/.config/hypr/scripts/toggle-hyprbars.sh
+++ configs/hypr/.config/hypr/scripts/toggle-hyprbars.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-if hyprpm list | grep -A1 "hyprbars" | grep "enabled: true"; then
-    hyprpm disable hyprbars
+
+STATE="$HOME/.cache/hyprbars_state"
+
+if [ -f "$STATE" ]; then
+    # ON
+    hyprctl keyword plugin:hyprbars:bar_height 15
+    hyprctl keyword plugin:hyprbars:bar_padding 8
+    hyprctl keyword plugin:hyprbars:bar_button_padding 4
+    hyprctl keyword plugin:hyprbars:bar_text_size 8
+    hyprctl keyword plugin:hyprbars:col.text rgb\(e8d5f0\)
+    rm "$STATE"
 else
-    hyprpm enable hyprbars
+    # OFF
+    hyprctl keyword plugin:hyprbars:bar_height 0
+    hyprctl keyword plugin:hyprbars:bar_padding 0
+    hyprctl keyword plugin:hyprbars:bar_button_padding 0
+    hyprctl keyword plugin:hyprbars:bar_text_size 0
+    hyprctl keyword plugin:hyprbars:col.text rgba\(00000000\)
+    touch "$STATE"
 fi
-hyprctl reload
